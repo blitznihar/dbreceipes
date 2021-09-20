a6937e56d3c04786623c76486bebf916d9359a6b
Nihar Malali <blitznihar@gmail.com>
2021-09-19 22:22:50 -0500

    Fixing unit tests

---
+++ src/test/java/com/blitznihar/restaturants/dbreceipes/integrationtests/RestaurantMongoControllerIT.java
@@ -0,0 +1,5 @@
+package com.blitznihar.restaturants.dbreceipes.integrationtests;
+
+public class RestaurantMongoControllerIT {
+    
+}
--- src/test/java/com/blitznihar/restaturants/dbreceipes/interationtests/RestaurantMongoControllerIT.java
+++
@@ -1,5 +0,0 @@
-package com.blitznihar.restaturants.dbreceipes.interationtests;
-
-public class RestaurantMongoControllerIT {
-    
-}
